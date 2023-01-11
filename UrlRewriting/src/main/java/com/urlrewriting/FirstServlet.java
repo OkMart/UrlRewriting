31e2bef12c8345fc5c7671e9d4896ed1188d54f5
satya <satya7512@gmail.com>
2023-01-11 14:47:08 +0530

    Commiting all the urlrewriting project

---
+++ UrlRewriting/src/main/java/com/urlrewriting/FirstServlet.java
@@ -0,0 +1,41 @@
+package com.urlrewriting;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String name = request.getParameter("name");
+		out.println("Welcome "+name);
+		
+		out.println("<a href='./servlet2?uname="+name+"'>Visit</a>");
+		
+		out.close();
+	}
+
+}
